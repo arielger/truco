47a9b0cc772bb4c4a82e071cf20968efef7fdb83
Ariel Gerstein <ariel.ger@hotmail.com>
2019-03-24 17:59:39 -0300

    Refactor styles of new match modal

--- client/src/screens/NewMatch/NewMatch.js
+++ client/src/screens/NewMatch/NewMatch.js
@@ -33,7 +33,8 @@ export default function NewMatch({ visible, onClose, client, history }) {
     <Modal
       isOpen={visible}
       onRequestClose={onClose}
-      className={styles["modal"]}
+      className={styles.modal}
+      overlayClassName={styles.overlay}
     >
       <h1 className={styles["title"]}>Nuevo partido</h1>
       <Formik
@@ -49,51 +50,67 @@ export default function NewMatch({ visible, onClose, client, history }) {
       >
         {({ values, isSubmitting }) => (
           <Form>
-            <div>
+            <div style={{ marginBottom: 16 }}>
               <span>Jugadores:</span>
-              <Field component="div" name="playersCount">
-                <input
-                  type="radio"
-                  id="2-players"
-                  defaultChecked={values.playersCount === 2}
-                  name="playersCount"
-                  value={2}
-                />
-                <label htmlFor="2-players">2 Jugadores</label>
-                <input
-                  type="radio"
-                  id="4-players"
-                  defaultChecked={values.playersCount === 4}
-                  name="playersCount"
-                  value={4}
-                />
-                <label htmlFor="4-players">4 Jugadores</label>
+              <Field
+                className={styles.field}
+                component="div"
+                name="playersCount"
+              >
+                <div className={styles.radioWrapper}>
+                  <input
+                    type="radio"
+                    id="2-players"
+                    defaultChecked={values.playersCount === 2}
+                    name="playersCount"
+                    value={2}
+                  />
+                  <label htmlFor="2-players">2 Jugadores</label>
+                </div>
+                <div className={styles.radioWrapper}>
+                  <input
+                    type="radio"
+                    id="4-players"
+                    defaultChecked={values.playersCount === 4}
+                    name="playersCount"
+                    value={4}
+                  />
+                  <label htmlFor="4-players">4 Jugadores</label>
+                </div>
               </Field>
             </div>
 
             <div>
               <span>Puntos</span>
-              <Field component="div" name="points">
-                <input
-                  type="radio"
-                  id="15-points"
-                  defaultChecked={values.points === 15}
-                  name="points"
-                  value={15}
-                />
-                <label htmlFor="15-points">15 Puntos</label>
-                <input
-                  type="radio"
-                  id="30-points"
-                  defaultChecked={values.points === 30}
-                  name="points"
-                  value={30}
-                />
-                <label htmlFor="30-points">30 Puntos</label>
+              <Field className={styles.field} component="div" name="points">
+                <div className={styles.radioWrapper}>
+                  <input
+                    type="radio"
+                    id="15-points"
+                    defaultChecked={values.points === 15}
+                    name="points"
+                    value={15}
+                  />
+                  <label htmlFor="15-points">15 Puntos</label>
+                </div>
+                <div className={styles.radioWrapper}>
+                  <input
+                    type="radio"
+                    id="30-points"
+                    defaultChecked={values.points === 30}
+                    name="points"
+                    value={30}
+                  />
+                  <label htmlFor="30-points">30 Puntos</label>
+                </div>
               </Field>
             </div>
 
-            <button type="submit" disabled={isSubmitting}>
+            <button
+              className={styles.createButton}
+              type="submit"
+              disabled={isSubmitting}
+            >
               Crear nueva partida
             </button>
           </Form>
